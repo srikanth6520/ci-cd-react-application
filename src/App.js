a202b18a84e1304aa5ba03f3ad1a0a4b8976e852
srikanth6520 <srikanth.kolluri@optit.in>
2025-04-28 17:32:11 +0530

    i have added code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, React World!</h1>
+      <p>This is a sample React application.</p>
+    </div>
+  );
+}
+
+export default App;
+
